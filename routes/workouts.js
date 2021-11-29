cb8448b4c323986548094af4f7e8028ca45a698f
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-29 10:59:57 -0600

    finished work out routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,2 +1,85 @@
 const router = require("express").Router();
 const Workouts = require("../models/workouts");
+
+router.get("/exercise", (req, res) => {
+  res.sendFile('./exercise.html', {root: "public" })
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile('./stats.html',  {root: "public"});
+});
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+      const dbWorkouts = await Workouts.aggregate( [
+          {
+              $addFields: {
+                  totalDuration: {
+                      $sum: "$exercises.duration"
+                  }
+              }
+          }
+      ] ).sort({ day: 1 });
+      console.log(dbWorkouts)
+      if (!dbWorkouts) {
+          res.status(400).json({message: "You have no workouts!"})
+      }
+      res.json(dbWorkouts);
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+router.post("/api/workouts", async (req, res) => {
+  try {
+      const newWorkout = new Workouts(req.body);
+      const dbWorkouts = await Workouts.create(newWorkout);
+      if (!dbWorkouts) {
+          res.status(404).json({ message: "Can't do it!"});
+      }
+      res.status(200).json(dbWorkouts);
+      
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+  try {
+      const updatedWorkout = await Workouts.updateOne(
+          { _id: req.params.id },
+          { $push: { "exercises": req.body } }
+      );
+      res.status(200).json(updatedWorkout);
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+      const workoutRange = await Workouts.aggregate( [
+          {
+              $addFields: {
+                  totalDuration: {
+                      $sum: "$exercises.duration"
+                  }
+              }
+          }
+      ] ).sort({ day: -1 }).limit(7);
+      console.log(workoutRange);
+      if (workoutRange) {
+          res.status(200).json(workoutRange)
+      } else {
+          res.status(404).json({message: "No Workouts!"})
+      }
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+module.exports = router;
