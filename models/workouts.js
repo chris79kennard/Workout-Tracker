47ae56e63b283ca50d59bf21afbf0725ecb736ab
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-29 10:58:30 -0600

    required express for work out routes

---
+++ models/workouts.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema({
+	day: {
+		type: Date,
+		default: Date.now()
+	},
+	exercises: [
+		{
+			type: {
+				type: String,
+				trim: true
+			},
+			name: {
+				type: String,
+				trim: true
+			},
+			duration: {
+				type: Number
+			},
+			weight: Number,
+			reps: Number,
+			sets: Number,
+			distance: Number
+		}
+	]
+});
+
+const Workouts = mongoose.model('Workouts', workoutSchema);
+
+module.exports = Workouts;
